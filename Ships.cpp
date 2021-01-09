34991fbfbf9eb670cbcf89d9cfee3446a6bd6d7c
PhilkaBO <ledentsov.vladislav.2014.b@gmail.com>
2021-01-09 16:34:48 +0700

    Create Ships.cpp

---
+++ Ships.cpp
@@ -0,0 +1,21 @@
+#include "Ships.h"
+
+int Сoordinates::GetX()
+{
+	return x;
+}
+
+int Сoordinates::GetY()
+{
+	return y;
+}
+
+void Сoordinates::SetX(int x)
+{
+	this->x = x;
+}
+
+void Сoordinates::SetY(int y)
+{
+	this->y = y;
+}
